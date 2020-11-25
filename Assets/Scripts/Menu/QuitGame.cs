c8dc0a491d15f4953ceb07b5a38e8f6129ba5bce
Claire <claire.chong@epitech.eu>
2020-11-26 00:00:43 +0100

    finished main features

---
+++ Assets/Scripts/Menu/QuitGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    public void OnClick()
+    {
+        Application.Quit();
+    }
+}
