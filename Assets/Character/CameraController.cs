e7b6443058e0d5d52a826ee8aa973d1019d4a9b8
Gwendal <gwendal.amosse@epitech.eu>
2020-11-27 19:15:57 +0100

    pig movement done

---
+++ Assets/Character/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraSmoothingFactor = 1.0f;
+    public float lookUpMax = 60;
+    public float lookUpMin = -15;
+
+    private Quaternion camRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camRotation = transform.localRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camRotation.y += Input.GetAxis("Mouse X") * cameraSmoothingFactor;
+        camRotation.x += Input.GetAxis("Mouse Y") * -cameraSmoothingFactor;
+
+        camRotation.x = Mathf.Clamp(camRotation.x, lookUpMin, lookUpMax); 
+
+        transform.localRotation = Quaternion.Euler(camRotation.x, camRotation.y, camRotation.z);
+    }
+}
